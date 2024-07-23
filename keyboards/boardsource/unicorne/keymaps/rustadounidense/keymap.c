bd62063b4a77880a72c963b7aa0aa139f9e47566
rustadounidense <>
2024-07-22 17:17:53 -0700

    move caps, esc

--- keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
@@ -13,8 +13,8 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   [_BASE] = LAYOUT_split_3x6_3(
-    KC_ESC,     KC_Q,               KC_W,               KC_E,                   KC_R,               KC_T,             /**/  KC_Y,                   KC_U,               KC_I,               KC_O,               KC_P,                   KC_BSLS,
-    KC_GRAVE,   MT(MOD_LGUI,KC_A),  MT(MOD_LALT,KC_S),  MT(MOD_LCTL,KC_D),      MT(MOD_LSFT,KC_F),  KC_G,             /**/  KC_H,                   MT(MOD_RSFT,KC_J),  MT(MOD_RCTL,KC_K),  MT(MOD_RALT,KC_L),  MT(MOD_RGUI,KC_SCLN),   KC_QUOTE,
+    KC_GRAVE,   KC_Q,               KC_W,               KC_E,                   KC_R,               KC_T,             /**/  KC_Y,                   KC_U,               KC_I,               KC_O,               KC_P,                   KC_BSLS,
+    KC_ESC,     MT(MOD_LGUI,KC_A),  MT(MOD_LALT,KC_S),  MT(MOD_LCTL,KC_D),      MT(MOD_LSFT,KC_F),  KC_G,             /**/  KC_H,                   MT(MOD_RSFT,KC_J),  MT(MOD_RCTL,KC_K),  MT(MOD_RALT,KC_L),  MT(MOD_RGUI,KC_SCLN),   KC_QUOTE,
     KC_NO,      KC_Z,               KC_X,               KC_C,                   KC_V,               KC_B,             /**/  KC_N,                   KC_M,               KC_COMMA,           KC_DOT,             KC_SLASH,               TO(_RU),
                                                         KC_NO,                  LT(_NUM,KC_SPACE),  LT(_FUN, KC_TAB), /**/  MT(MOD_HYPR, KC_ENTER), LT(_NAV,KC_BSPC),   KC_NO
   ),
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   [_NUM] = LAYOUT_split_3x6_3(
     LCTL(KC_6), LCTL(KC_1),         LCTL(KC_2),         LCTL(KC_3),             LCTL(KC_4),         LCTL(KC_5),       /**/  KC_LBRC,                KC_7,               KC_8,               KC_9,               KC_EQUAL,               KC_RBRC,
-    KC_NO,      OSM(MOD_LGUI),      OSM(MOD_LALT),      OSM(MOD_LCTL),          OSM(MOD_LSFT),      KC_NO,                  KC_LPRN,                KC_1,               KC_2,               KC_3,               KC_MINUS,               KC_RPRN,
-    KC_NO,      KC_NO,              KC_NO,              KC_NO,                  KC_NO,              KC_NO,                  KC_LT,                  KC_4,               KC_5,               KC_6,               KC_SLASH,               KC_GT,
+    KC_NO,      OSM(MOD_LGUI),      OSM(MOD_LALT),      OSM(MOD_LCTL),          OSM(MOD_LSFT),      CW_TOGG,                KC_LPRN,                KC_1,               KC_2,               KC_3,               KC_MINUS,               KC_RPRN,
+    KC_NO,      KC_NO,              KC_NO,              KC_NO,                  KC_NO,              KC_CAPS,                KC_LT,                  KC_4,               KC_5,               KC_6,               KC_SLASH,               KC_GT,
                                                         KC_TRNS,                KC_TRNS,            KC_TRNS,                KC_DOT,                 KC_0,               KC_TRNS
   ),
   // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
