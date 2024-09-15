dd2272dad9364d7523a8e803678822bcee559c83
rustadounidense <>
2024-09-15 12:30:27 -0700

    move brackets

--- keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUM] = LAYOUT_split_3x6_3(
     KC_TRNS,    KC_LPRN,            KC_LCBR,            KC_RCBR,                KC_RPRN,            KC_NO,            /**/  KC_GRAVE,               KC_7,               KC_8,                 KC_9,               KC_EQUAL,               KC_TRNS,
     KC_TRNS,    OSM(MOD_LGUI),      OSM(MOD_LALT),      OSM(MOD_LCTL),          OSM(MOD_LSFT),      CW_TOGG,                KC_DOT,                 KC_1,               KC_2,                 KC_3,               KC_MINUS,               KC_TRNS,
-}    KC_TRNS,    KC_LBRC,            KC_LABK,            KC_RABK,                KC_RBRC,            KC_CAPS,                KC_BSLS,                KC_4,               KC_5,                 KC_6,               KC_SLASH,               KC_TRNS,
+    KC_TRNS,    KC_LBRC,            KC_LABK,            KC_RABK,                KC_RBRC,            KC_CAPS,                KC_BSLS,                KC_4,               KC_5,                 KC_6,               KC_SLASH,               KC_TRNS,
                                                         KC_TRNS,                KC_TRNS,            KC_TRNS,                KC_TRNS,                LT(_NAV,KC_0),      KC_TRNS
   ),
   // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
