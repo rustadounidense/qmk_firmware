e56a5687d2044e8e7fb77ad5501dad6dd11c657a
rustadounidense <>
2025-01-08 19:43:26 -0800

    move sym layer toggle to thumbs

--- keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
@@ -15,8 +15,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_EN] = LAYOUT_split_3x6_3(
     KC_GRAVE ,    KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,             /**/  KC_Y,                   KC_U,                 KC_I,                 KC_O,               KC_P,                  KC_SLASH,
     CW_TOGG,      MT(MOD_LGUI,KC_A),  MT(MOD_LALT,KC_S),  MT(MOD_LCTL,KC_D),  MT(MOD_LSFT,KC_F),  KC_G,             /**/  KC_H,                   MT(MOD_RSFT,KC_J),    MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  MT(MOD_RGUI,KC_QUOTE), KC_MINUS,
-    KC_NO    ,    KC_Z,               KC_X,               LT(_SYM,KC_C),      KC_V,               KC_B,             /**/  KC_N,                   KC_M,                 LT(_SYM,KC_COMMA),    KC_DOT,             KC_SCLN,               TO(_RU),
-                                                          KC_ESCAPE,          LT(_NUM,KC_SPACE),  LT(_FUN, KC_TAB), /**/  MT(MOD_HYPR,KC_ENTER),  LT(_NAV,KC_BSPC),     LT(_KBD,KC_CAPS)
+    KC_NO    ,    KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,             /**/  KC_N,                   KC_M,                 LT(_SYM,KC_COMMA),    KC_DOT,             KC_SCLN,               TO(_RU),
+                                                          LT(_FUN,KC_ESCAPE), LT(_NUM,KC_SPACE),  LT(_SYM,KC_TAB),  /**/  LT(_SYM,KC_ENTER),      LT(_NAV,KC_BSPC),     LT(_KBD,KC_CAPS)
   ),
   // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   [_RU] = LAYOUT_split_3x6_3(
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   [_NUM] = LAYOUT_split_3x6_3(
     LCTL(KC_6),   LCTL(KC_1),         LCTL(KC_2),         LCTL(KC_3),         LCTL(KC_4),         LCTL(KC_5),       /**/  KC_EQUAL,               KC_7,                 KC_8,                 KC_9,               KC_ASTR,              KC_SLASH,
-    KC_NO,        OSM(MOD_LGUI),      OSM(MOD_LALT),      OSM(MOD_LCTL),      OSM(MOD_LSFT),      KC_NO,                  KC_0,                   KC_1,                 KC_2,                 KC_3,               KC_PLUS,              KC_MINUS,
+    OSM(MOD_HYPR),OSM(MOD_LGUI),      OSM(MOD_LALT),      OSM(MOD_LCTL),      OSM(MOD_LSFT),      KC_NO,                  KC_0,                   KC_1,                 KC_2,                 KC_3,               KC_PLUS,              KC_MINUS,
     KC_NO,        KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,                  KC_COMMA,               KC_4,                 KC_5,                 KC_6,               KC_DOT,               KC_UNDS,
                                                           KC_NO,              KC_NO,              KC_NO,                  KC_TRNS,                KC_TRNS,              KC_TAB
   ),
