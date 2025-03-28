379ab2aaedd0e4dec414c8479481127add3406ff
rustadounidense <>
2025-03-27 19:48:31 -0700

    pinky slash

--- keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/rustadounidense/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // =============================================================================================================================================================================================================================================================
   [_EN] = LAYOUT_split_3x6_3(
     KC_GRAVE,       KC_Q,                   KC_W,                   KC_E,                   KC_R,               KC_T,             /**/  KC_Y,                   KC_U,                 KC_I,                 KC_O,               KC_P,                  KC_MINUS,
-    KC_BSLS,        MT(MOD_LGUI,KC_A),      MT(MOD_LALT,KC_S),      MT(MOD_LCTL,KC_D),      MT(MOD_LSFT,KC_F),  KC_G,             /**/  KC_H,                   MT(MOD_RSFT,KC_J),    MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  MT(MOD_RGUI,KC_QUOTE), KC_SLASH,
+    KC_BSLS,        MT(MOD_LGUI,KC_A),      MT(MOD_LALT,KC_S),      MT(MOD_LCTL,KC_D),      MT(MOD_LSFT,KC_F),  KC_G,             /**/  KC_H,                   MT(MOD_RSFT,KC_J),    MT(MOD_RCTL,KC_K),    MT(MOD_RALT,KC_L),  MT(MOD_RGUI,KC_SLASH), KC_QUOTE,
     KC_NO,          KC_Z,                   KC_X,                   KC_C,                   KC_V,               KC_B,             /**/  KC_N,                   KC_M,                 KC_COMMA,             KC_DOT,             KC_SCLN,               TO(_RU),
                                                                     LT(_FUN,KC_ESC),        LT(_NUM,KC_SPACE),  LT(_SYM,KC_TAB),  /**/  MT(MOD_HYPR,KC_ENTER),  LT(_NAV,KC_BSPC),     MO(_KBD)
   ),
@@ -28,14 +28,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // =============================================================================================================================================================================================================================================================
   [_NUM] = LAYOUT_split_3x6_3(
     LCTL(KC_6),     LCTL(KC_1),             LCTL(KC_2),             LCTL(KC_3),             LCTL(KC_4),         LCTL(KC_5),       /**/  KC_EQUAL,               KC_7,                 KC_8,                 KC_9,               KC_PLUS,              KC_MINUS,
-    OSM(MOD_HYPR),  OSM(MOD_LGUI),          OSM(MOD_LALT),          OSM(MOD_LCTL),          OSM(MOD_LSFT),      LCTL(KC_6),       /**/  KC_DOT,                 KC_1,                 KC_2,                 KC_3,               KC_ASTR,              KC_SLASH,
+    OSM(MOD_HYPR),  OSM(MOD_LGUI),          OSM(MOD_LALT),          OSM(MOD_LCTL),          OSM(MOD_LSFT),      LCTL(KC_6),       /**/  KC_DOT,                 KC_1,                 KC_2,                 KC_3,               KC_SLASH,             KC_ASTR,
     KC_NO,          KC_NO,                  KC_NO,                  KC_NO,                  KC_NO,              KC_NO,            /**/  KC_COMMA,               KC_4,                 KC_5,                 KC_6,               KC_SCLN,              KC_NO,
                                                                     KC_NO,                  KC_NO,              KC_NO,            /**/  KC_TRNS,                KC_0,                 KC_NO
   ),
   // =============================================================================================================================================================================================================================================================
   [_SYM] = LAYOUT_split_3x6_3(
     KC_NO,          KC_NO,                  KC_NO,                  KC_NO,                  KC_NO,              KC_NO,            /**/  KC_EQUAL,               KC_PIPE,              KC_LCBR,              KC_RCBR,            KC_PLUS,             KC_MINUS,
-    KC_NO,          KC_LGUI,                KC_LOPT,                KC_LCTL,                KC_LSFT,            KC_NO,            /**/  KC_DOT,                 KC_LABK,              KC_LPRN,              KC_RPRN,            KC_ASTR,             KC_SLASH,
+    KC_NO,          KC_LGUI,                KC_LOPT,                KC_LCTL,                KC_LSFT,            KC_NO,            /**/  KC_DOT,                 KC_LABK,              KC_LPRN,              KC_RPRN,            KC_SLASH,            KC_ASTR,
     KC_NO,          KC_NO,                  KC_NO,                  KC_NO,                  KC_NO,              KC_NO,            /**/  KC_COMMA,               KC_RABK,              KC_LBRC,              KC_RBRC,            KC_SCLN,             KC_NO,
                                                                     KC_TRNS,                KC_TRNS,            KC_TRNS,          /**/  KC_TRNS,                KC_TRNS,              KC_TRNS
   ),
